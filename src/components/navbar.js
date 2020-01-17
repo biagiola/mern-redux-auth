5bd9db7dc566a5854c780b5e9ec8b3913df44b67
David <41311088+biagiola@users.noreply.github.com>
2020-01-17 10:47:47 -0300

    Add files via upload

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,24 +2,23 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
-    
     render() {
         return (
-            <nav className="navbar navbar-light bg-light navbar-expand-sm">
-                <Link to="/" className="navbar-brand">Schneller</Link>
+            <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
+                <Link to="/" className="navbar-brand">ExcerTracker</Link>
                 <div className="collpase navbar-collapse">
                     <ul className="navbar-nav mr-auto">
                         <li className="navbar-item">
-                            <Link to="/" className="nav-link">Articles</Link>
+                            <Link to="/" className="nav-link">Exercises</Link>
                         </li>
                         <li className="navbar-item">
-                            <Link to="/create" className="nav-link">Create Article</Link>
+                            <Link to="/create" className="nav-link">Create Exercise Log</Link>
                         </li>
                         <li className="navbar-item">
-                            <Link to="/user" className="nav-link">Add Lenguage</Link>
+                            <Link to="/user" className="nav-link">Create User</Link>
                         </li>
                     </ul>
-                </div><br/>
+                </div>
             </nav>
         );
     }
