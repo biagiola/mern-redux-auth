8afe3c36a0b65515431693849943985458c13849
David <41311088+biagiola@users.noreply.github.com>
2020-01-17 10:21:45 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,25 +4,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/navbar';
-import ExersiceList from './components/exercises-list';
-import EditExercise from './components/edit-exercise';
-import DetailsExercise from './components/details-exercise';
-import CreateExercise from './components/create-exercise';
-import CreateUser from './components/create-user';
+import ArticlesList from './components/article-list';
+import ArticleDetails from './components/article-details';
+import CreateArticle from './components/create-article';
+import EditArticle from './components/edit-article';
+import DeleteArticle from './components/delete-article';
+import CreateLenguage from './components/create-lenguage';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App text-white">
         <BrowserRouter>
             <Navbar/>
-            <br/>
-            <Switch>
-                <Route exact path="/" component={ ExersiceList } />
-                <Route path="/edit/:id" component={ EditExercise } />
-                <Route path="/details/:id" component={ DetailsExercise } />
-                <Route path="/create" component={ CreateExercise } />
-                <Route path="/user" component={ CreateUser } />
-            </Switch>
+            <main>
+                <Switch>
+                    <Route exact path="/" component={ ArticlesList } />
+                    <Route path="/details/:id" component={ ArticleDetails } />
+                    <Route path="/create" component={ CreateArticle } />
+                    <Route path="/edit/:id" component={ EditArticle } />
+                    <Route path="/deleted" component={ DeleteArticle } />
+                    <Route path="/user" component={ CreateLenguage } />
+                </Switch>
+            </main>
         </BrowserRouter>
     </div>
   );
