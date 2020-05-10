d0c8af1a4491c55e67cf6fe6f01f7e4e3b3bb8ce
David <41311088+biagiola@users.noreply.github.com>
2020-05-09 21:41:45 -0400

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -10,20 +10,25 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect( uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}
-);
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('MongoDB database connection extablished successfully');
-})
+const uri = 'mongodb://localhost/test1';
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false });
+
+mongoose.connection.once('open', function(){
+    console.log('Connection has been made, now make fireworks...');
+}).on('error', function(error){
+    console.log('Connection error:', error);
+});    
 
 const articlesRouter = require('./routes/articles');
 const lenguageRouter = require('./routes/lenguages');
+const authRoute = require('./routes/auth');
 app.use('/articles', articlesRouter);
 app.use('/lenguages', lenguageRouter);
+app.use('/auth', authRoute);
 
 // express is listening the port
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
