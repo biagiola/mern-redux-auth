3d0380d2636cafe3fc22555a5df0c1c6f1e21a09
David <41311088+biagiola@users.noreply.github.com>
2020-06-20 19:10:02 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
@@ -12,26 +12,33 @@ import DeleteArticle from './components/delete-article';
 import CreateLenguage from './components/create-lenguage';
 import CreateUser from './components/create-user';
 import SignUp from './components/sign-up';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App text-white">
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={ SignUp } />
-                <Navbar/>
-                <Route path="/dashboard" component={ ArticlesList } />
-                <Route path="/details/:id" component={ ArticleDetails } />
-                <Route path="/create" component={ CreateArticle } />
-                <Route path="/edit/:id" component={ EditArticle } />
-                <Route path="/deleted" component={ DeleteArticle } />
-                <Route path="/lenguage" component={ CreateLenguage } />
-                <Route path="/signup" component={ CreateUser } />
-            </Switch>
-        </BrowserRouter>
-    </div>
-  );
+export default class App extends Component {
+
+  componentDidMount(){
+    axios.post('http://localhost:5000/auth/register0', )
+  }
+
+  render(){
+    return (
+      <div className="App text-white">
+          <BrowserRouter>
+              { false && <Navbar/>     }
+              <Switch>
+                  <Route exact path="/" component={ SignUp } />
+                  <Route path="/dashboard" component={ ArticlesList } />
+                  <Route path="/details/:id" component={ ArticleDetails } />
+                  <Route path="/create" component={ CreateArticle } />
+                  <Route path="/edit/:id" component={ EditArticle } />
+                  <Route path="/deleted" component={ DeleteArticle } />
+                  <Route path="/lenguage" component={ CreateLenguage } />
+                  <Route path="/signup" component={ CreateUser } />
+              </Switch>
+          </BrowserRouter>
+      </div>
+    );
+  }
+  
 }
-
-export default App;
 
