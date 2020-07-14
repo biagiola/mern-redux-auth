913253740850bec649d7548d5786b253d24dcc05
David <41311088+biagiola@users.noreply.github.com>
2020-07-14 07:51:27 -0400

    Add files via upload

--- backendLocal/routes/lenguages.js
+++ backendLocal/routes/lenguages.js
@@ -1,22 +1,39 @@
+const express = require('express')
+const app = express()
 const router = require('express').Router();
 let Lenguage = require('../models/lenguage.model');
 
+router.get('/', (req, res) => {
+    Lenguage.find()
+        .then( lenguages => res.json(lenguages))
+        .catch( err => res.status(400).json( 'Error: ' + err))
+})
 
+router.post('/add', (req, res) => {
+    const lenguage = req.body.lenguage
+    const newLenguage = new Lenguage( { lenguage})
+
+    newLenguage.save()
+        .then( () => res.json('Lenguage added!'))
+        .catch( err => res.status(400).json( 'Error' + err))
+})
+
+module.exports = router;
+
+/*
 router.route('/').get( (req, res) => {
     Lenguage.find()
-        .then( lenguages => res.json(lenguages) )
-        .catch( err => res.status(400).json( 'Error: ' + err) );
+    .then( lenguages => res.json(lenguages) )
+    .catch( err => res.status(400).json( 'Error: ' + err) );
 });
 
 router.route('/add').post( (req, res) => {
     const lenguage = req.body.lenguage;
-
+    
     const newLenguage = new Lenguage( { lenguage } );
-
+    
     newLenguage.save()
-        .then( () => res.json('Lenguage added!') )
-        .catch( err => res.status(400).json( 'Error: ' + err ) );
-
-});
-
-module.exports = router;
+    .then( () => res.json('Lenguage added!') )
+    .catch( err => res.status(400).json( 'Error: ' + err ) );
+    
+});*/
