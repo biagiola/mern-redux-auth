a2e34a7fb2d1d472518a5e111988fa5b60d87f75
David <41311088+biagiola@users.noreply.github.com>
2020-08-15 18:50:00 -0400

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { PropTypes } from 'prop-types';
-import { connect } from 'react-redux';
-import { changeShowNavbar } from '../actions';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { PropTypes } from 'prop-types'
+import { connect } from 'react-redux'
+import { changeShowNavbar } from '../actions'
 
 
 class Navbar extends Component {
