229ada453539fdca670230c9cab452f5a7e62263
David <41311088+biagiola@users.noreply.github.com>
2020-08-15 18:40:17 -0400

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ export const changeShowNavbar = () => dispatch => {
     console.log('actions')
     dispatch({
         type: 'SWITCH_NAVBAR',
-    });
+    })
 }
 
 export const setUsername = name => dispatch => {
@@ -17,5 +17,5 @@ export const articlesGone = () => dispatch => {
     console.log('actions articlesGone')
     dispatch({
         type: 'ARTICLES_GONE'
-    });
+    })
 }
