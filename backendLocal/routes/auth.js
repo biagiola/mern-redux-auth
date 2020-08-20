cabeeb25ab7c593d0be828e1040e2c0c6688b20c
David <41311088+biagiola@users.noreply.github.com>
2020-08-20 10:05:04 -0400

    hash password added and authtoken

--- backendLocal/routes/auth.js
+++ backendLocal/routes/auth.js
@@ -2,26 +2,78 @@ const router = require('express').Router();
 let User = require('../models/user.model');
 const { registerValidation, loginValidation } = require('../../src/validation');
 const Joi = require('@hapi/joi');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+router.get('/hola', (req, res) => {
+  return res.status(200).send('funcionó')
+})
 
 router.post('/login', async (req, res) => {
-    //Lets validate the data
-    const schema = Joi.object({
-        email: Joi.string().min(6).required().email(),
-        password: Joi.string().min(6).required()
+  //Lets validate the data
+  const schema = Joi.object({
+      email: Joi.string().min(6).required().email(),
+      password: Joi.string().min(6).required()
+  });
+  const { error } = schema.validate(req.body);
+  if(error) return res.status(400).send(error.details[0].message)
+  
+  User.find({ email: req.body.email })
+  .exec()
+  .then(user => {
+    if (user.length < 1) {
+      return res.status(401).json({
+        message: "Auth failed"
+      });
+    }
+    bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+      if (err) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      if (result) {
+        const token = jwt.sign(
+          {
+            email: user[0].email,
+            userId: user[0]._id
+          },
+          'secret',
+          {
+            expiresIn: "1h"
+          }
+        );
+        return res.status(200).json({
+          message: "Auth successful",
+          name: user[0].name,
+          token: token
+        });
+      }
+      res.status(401).json({
+        message: "Auth failed"
+      });
     });
-    const { error } = schema.validate(req.body);
-    if(error) return res.status(400).send(error.details[0].message)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: err
+    });
+  });
 
-    User.findOne({ email: req.body.email })
-        .exec()
-        .then( response => {
-            console.log('user found, response: ', response)
-            res.status(200).send('Email was found')
+    // search the data in the db
+    /*User.findOne({ email: req.body.email })
+      .exec()
+      .then( response => {
+        console.log('user found, response: ', response)
+        res.status(200).json({ 
+          response: response,
         })
-        .catch( err => {
-            res.status(404).send('User not found')
-        })
-
+      })
+      .catch( err => {
+        res.status(404).send('User not found')
+      })
+    */
 });
 
 router.post('/register', async (req, res) => {
@@ -45,7 +97,35 @@ router.post('/register', async (req, res) => {
         password: req.body.password
     });
 
-    try{
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+          return res.status(500).json({
+            error: err
+          });
+        } else {
+          const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hash
+          });
+          user
+            .save()
+            .then(result => {
+              console.log(result);
+              res.status(201).json({
+                message: "User created"
+              });
+            })
+            .catch(err => {
+              console.log(err);
+              res.status(500).json({
+                error: err
+              });
+            });
+        }
+    })
+
+    /*try{
         const savedUser = await user.save();
         console.log('auth.js /register', savedUser);
         savedUser.message = "user has been saved";
@@ -53,7 +133,7 @@ router.post('/register', async (req, res) => {
     }catch( err ) {
         console.log('auth.js ERROR');
         res.status(404).send(err);
-    }
+    }*/
 });
 
 module.exports = router;
