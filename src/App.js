1067c4aadfeedfccf634ab220dc638731c66de37
David <41311088+biagiola@users.noreply.github.com>
2020-08-22 12:43:41 -0400

    routes modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { connect } from 'react-redux'
-import PropTypes from 'prop-types'
 
 import Navbar from './components/navbar'
+import Login from './components/login'
 import ArticlesList from './components/article-list'
 import ArticleDetails from './components/article-details'
 import CreateArticle from './components/create-article'
@@ -13,20 +13,19 @@ import EditArticle from './components/edit-article'
 import DeleteArticle from './components/delete-article'
 import CreateLenguage from './components/create-lenguage'
 import CreateUser from './components/create-user'
-import SignUp from './components/sign-up'
 
 import {
   CSSTransition,
   TransitionGroup,
 } from 'react-transition-group'
 
-class App extends Component {
+export default class App extends Component {
 
   render(){
     return (
       <div className="App text-white">
           <BrowserRouter>
-              { this.props.showNavbar && <Navbar/> }
+              <Navbar/>
               <Route render={( { location } ) => (
                     <TransitionGroup>
                         <CSSTransition
@@ -37,8 +36,8 @@ class App extends Component {
                           classNames="fade"
                         >
                           <Switch location={ location }>
+                            <Route exact path="/" component={ Login } />
                             <Route path="/dashboard" component={ ArticlesList } />
-                            <Route exact path="/" component={ SignUp } />
                             <Route path="/details/:id" component={ ArticleDetails } />
                             <Route path="/create" component={ CreateArticle } />
                             <Route path="/edit/:id" component={ EditArticle } />
@@ -54,15 +53,4 @@ class App extends Component {
       </div>
     );
   }
-  
 }
-
-App.propTypes = {
-  showNavbar: PropTypes.bool
-}
-
-const mapStateToProps = state => ({
-  showNavbar: state.casa.switchNavbar
-})
-
-export default connect(mapStateToProps, null)(App);
