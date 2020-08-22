8f676531ec70783217f235ff64bb4efdf8a87bde
David <41311088+biagiola@users.noreply.github.com>
2020-08-22 12:53:41 -0400

    last update

--- src/reducers/reducerOne.js
+++ src/reducers/reducerOne.js
@@ -1,37 +1,31 @@
 const initialState = {
-    switchNavbar: false,
-    logout: false,
-    gone: false,
     username: '',
-    authToken: null
+    authToken: null,
+    gone: false
 }
 
 export default function(state = initialState, action) {
     switch(action.type){
-        case 'SWITCH_NAVBAR':
-            console.log('SWITCH_NAVBAR')
-            return {
-                ...state,
-                switchNavbar: !state.switchNavbar
-            }
         case 'SET_AUTH_TOKEN':
         console.log('SET_AUTH_TOKEN')
         return {
             ...state,
             authToken: action.payload
         }
+
         case 'SET_USER_NAME':
-            console.log('SET_USER_NAME')
-            return {
-                ...state,
-                username: action.payload
-            }
+        console.log('SET_USER_NAME')
+        return {
+            ...state,
+            username: action.payload
+        }
+
         case 'ARTICLES_GONE':
-            return {
-                ...state,
-                gone: true
-            }            
+        return {
+            ...state,
+            gone: true
+        }            
         default:
-            return state;
+        return state;
     }
 }
