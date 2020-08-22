812e3d77c9c901f3017cee1dec53a9ab708a7520
David <41311088+biagiola@users.noreply.github.com>
2020-08-22 12:17:49 -0400

    enviroments setting on nodemon.json added

--- backendLocal/server.js
+++ backendLocal/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const session = require('express-session')
 const app = express();
-const MongoStore = require('connect-mongo')(session)
-const mongoose = require('mongoose');
+
+const bodyParser = require("body-parser");
 const cors = require('cors');
+
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')(session)
 
 // set port and env
 require('dotenv').config();
@@ -16,12 +19,14 @@ mongoose.connection.once('open', function(){
     console.log('Connection has been made, now make fireworks...');
 }).on('error', function(error){
     console.log('Connection error:', error);
-});  
+}); 
 
 // middlewares
-app.use(express.urlencoded({extended: true})); //bodyparser
-app.use(cors());
 app.use(express.json());
+//app.use(express.urlencoded({extended: true})); //bodyparser
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // node-session setup
 app.use(session({
@@ -33,33 +38,52 @@ app.use(session({
     cookie: {
         maxAge: 1000 * 60 * 60 * 2 , //maxime time for a session 
         sameSite: true, //stric
-        secure: 'development' === 'production' //in our case, this will be false
+        secure: false //in our case, this will be false
     }
 }))
 
 // this middleware will execute everytime
 app.use((req, res, next) => {
+    //console.log('server.js req.body.email', req.body.email)
+    //console.log('server.js req.session', req.session)
+    //console.log('server.js req.session', req.session)
     const { userId } = req.session
-    console.log('hola: userId', req.session)
+
     if(userId) {
+        console.log('testing00')
         res.locals.user = users.find(
             user => user.id === userId
         )
     }
-    console.log('hola: res.locals.user', res.locals.user)
+    //console.log('server.js res.locals.user', res.locals.user)
     next()
 })
+
+// headers configuration
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+    }
+    next();
+});
 
 // Routes
 const articlesRouter = require('./routes/articles');
 const lenguageRouter = require('./routes/lenguages');
-const authRoute = require('./routes/auth');
+const authRouter = require('./routes/auth');
 app.use('/articles', articlesRouter);
 app.use('/lenguages', lenguageRouter);
-app.use('/auth', authRoute);
+app.use('/auth', authRouter);
 
 // express is listening the port
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+app.listen(port, () => 
+    console.log(`Server is running on port ${port}`
+));
 
