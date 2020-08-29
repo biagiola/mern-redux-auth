e728abd6518504c95776eb347d2fd58654dfe89f
David <41311088+biagiola@users.noreply.github.com>
2020-08-29 15:40:35 -0400

    Add files via upload

--- src/components/delete-article.js
+++ src/components/delete-article.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { PropTypes } from 'prop-types'
 import { connect } from 'react-redux'
-//import { articlesGone } from '../actions'
 
 class DeleteArticle extends Component {
     constructor(props){
@@ -26,13 +25,16 @@ class DeleteArticle extends Component {
         const empty = console.log('render', this.state.articles)
         if(!this.state.articles.length) {
             console.log('isArticlesEmpty')
-            this.props.articlesGone()
         }
 
         return ( 
-            <div className="wrapper container">
-                <div className="text-dark">Article was deleted</div><br/>
-                <Link to={'/dashboard'} onClick={ () => this.isArticlesEmpty }className="btn btn-primary">Go back</Link>
+            <div className="wrapper content">
+                <div>Article was deleted</div><br/>
+                <Link 
+                    to={'/dashboard'} 
+                    onClick={ () => this.isArticlesEmpty }
+                    className="btn btn-primary"
+                >Go back</Link>
                 { empty }
             </div>
             
