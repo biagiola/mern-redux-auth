1580d789836e62ae852694001327528545344f4a
David <41311088+biagiola@users.noreply.github.com>
2020-09-11 10:34:10 -0400

    class base changed to react hooks

--- src/components/article-details.js
+++ src/components/article-details.js
@@ -1,58 +1,32 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect  } from 'react'
 import { Link, Redirect } from 'react-router-dom'
-
-import { connect } from 'react-redux'
-import { PropTypes } from 'prop-types'
-
+import { useSelector } from 'react-redux'
+import { withRouter } from 'react-router'
 import axios from 'axios'
 
-class ArticleDetails extends Component {
-  constructor(props) {
-  super(props)
-    this.state = {
-      title: '',
-      description: '',
-      duration: 0,
-      date: new Date(),
-      lenguages: [],
-      articles: [],
-      showTitle: '',
-      id: '',
-    }
-  }
+function ArticleDetails(props) {
+  const authToken = useSelector(store => store.main.authToken)
+  const [title, setTitle] = useState('')
+  const [ description, setDescription ] = useState('')
+  const [ date, setDate ] = useState()
+  const [ lenguage, setLenguage ] = useState()
 
-  componentDidMount() {
-
-    this.setState({
-      id: this.props.match.params.id 
-    })
-
-    // get individual exercise.
-    axios.get('http://localhost:5000/articles/' + this.props.match.params.id)
+  useEffect( () => {
+    axios.get('http://localhost:5000/articles/' + props.match.params.id)
     .then(response => {
-      this.setState({
-        title: response.data.title,
-        description: response.data.description,
-        duration: response.data.duration,
-        date: new Date(response.data.date)
-      }) 
+      console.log('useEffect response', response)
+      setTitle(response.data.title)
+      setDescription(response.data.description)
+      setDate(new Date(response.data.date))
+      setLenguage(response.data.lenguage) 
     })
-    .catch(function (error) {
-      console.log(error);
-    })
+    .catch((error) => console.log(error))
 
-      // get all lenguages.
-    axios.get('http://localhost:5000/lenguages/')
-    .then(response => {
-      if (response.data.length > 0) {
-        this.setState({
-          lenguages: response.data.map(lenguage => lenguage.lenguage),
-        })
-      }
-  }).catch( error => console.log(error) )
-  }
+    return () => {}
 
-  deleteArticle = id => {
+  }, [authToken] )
+
+  const deleteArticle = id => {
     axios.delete( 'http://localhost:5000/articles/' + id )
       .catch( res => console.log( res.data ) )
     
@@ -60,50 +34,38 @@ class ArticleDetails extends Component {
       articles: this.state.articles.filter( el => el._id !== id )  
     })
   }
+  
+  const main = authToken !== null ?
+  <div>
+    <h3 className="text-center">{ title }</h3>
 
-  render() {
-    const main = this.props.authToken !== null ?
-    <div>
-      <h3 className="text-center">{ this.state.title }</h3>
-
-      <div className="container" style={{ 'padding': '0px 0px 0px', 'background' : '#d6d8db'}}>
-        {this.state.description}
-        <small style={ {'padding': '50px 0px 0px', } }>Written at: { this.state.date.toString().substr(0,10) }</small>
-      </div>
-
-      <div style={{ 'marginTop': '2rem'}}>
-        <Link to={ '/edit/' + this.props.match.params.id } className="btn">edit</Link> 
-        <Link to={"/deleted"} className="btn" onClick={ () =>{ this.deleteArticle(this.props.match.params.id) } }>delete</Link> 
-        <Link to={"/dashboard"} className="btn">back</Link>
-      </div>
-        
-    </div>
-    :
-    <div>
-      <Redirect to="/" />
+    <div className="container" style={{ 'padding': '0px 0px 0px', 'background' : '#d6d8db'}}>
+      { description }
+      <div>{ lenguage }</div>
+      <small style={{ 'padding': '50px 0px 0px' }}>Written at: { date.toString().substr(0,10) }</small>
     </div>
 
-    const value = this.props.moveContentValue ?
-    "60px" :  "250px"
+    <div style={{ 'marginTop': '2rem'}}>
+      <Link to={ '/edit/' + props.match.params.id } className="btn">edit</Link> 
+      <Link to={"/deleted"} className="btn" onClick={ () =>{ deleteArticle(props.match.params.id) } }>delete</Link> 
+      <Link to={"/dashboard"} className="btn">back</Link>
+    </div>
+      
+  </div>
+  :
+  <div>
+    <Redirect to="/" />
+  </div>
 
-    return (
-      <div className="wrapper content" style={{ marginLeft: value }}>
-        { main }
-      </div>
-    )
-  }
-}   
+  const moveContentValue = useSelector(store => store.main.moveContentValue)
+  const margin = moveContentValue ?
+  "60px" :  "250px"
 
-ArticleDetails.propTypes = {
-  username: PropTypes.string,
-  authToken: PropTypes.string,
-  moveContentValue: PropTypes.bool,
+  return (
+    <div className="wrapper content" style={{ marginLeft: margin }}>
+      { main }
+    </div>
+  )
 }
   
-const mapStateToProps = state => ({
-  username: state.main.username,
-  authToken: state.main.authToken,
-  moveContentValue: state.main.moveContentValue
-})
-  
-export default connect(mapStateToProps, null)(ArticleDetails)
+export default withRouter(ArticleDetails)
