9693fa37a40c6b0996f3654099b54ba20be840b1
David <41311088+biagiola@users.noreply.github.com>
2020-09-11 09:04:35 -0400

    Add files via upload

--- src/components/create-article.js
+++ src/components/create-article.js
@@ -1,155 +1,117 @@
-import React, { Component } from 'react'
-
+import React, { useState, useEffect, useRef } from 'react'
+import { Link, Redirect } from 'react-router-dom'
+import { withRouter } from 'react-router'
+import { useSelector } from 'react-redux'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 import axios from 'axios'
 
-import { connect } from 'react-redux'
-import { PropTypes } from 'prop-types'
+function CreateArticle(props) {
+  const lenguageRef = useRef('')
+  const [lenguages, setLenguages] = useState([])
+  const [lenguage, setLenguage] = useState('')
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [date, setDate] = useState('')
 
+  useEffect( () => {
+      axios.get('http://localhost:5000/lenguages/')
+      .then( response => {
+          if ( response.data.length > 0 ) {
+            setLenguages(response.data.map( lenguages => lenguages.lenguage ))
+          }
+        }
+      ).catch( error => console.log(error))
 
-class CreateArticle extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      lenguage: '',
-      title: '',
-      description: '',
-      date: new Date(),
-      lenguages: []
-    }
-  }
-  
-  componentDidMount() {
-    axios.get('http://localhost:5000/lenguages/')
-    .then( response => {
-      if ( response.data.length > 0 ) {
-        this.setState({
-          lenguages: response.data.map( lenguages => lenguages.lenguage ),
-          lenguage: response.data[0].lenguage
-        })
+      return () => {
+        
       }
-    }
-    ).catch( error => console.log(error) )
-  }
 
-  onChangeLenguage = e => {
-    this.setState({
-      lenguage: e.target.value
-    })
-  }
-
-  onChangeTitle = e => {
-    this.setState({
-      title: e.target.value
-    })
-  }
-
-  onChangeDescription = e => {
-    this.setState({
-      description: e.target.value
-    })
-  }
-
-  onChangeDate = date => {
-    this.setState({
-      date: date
-    })
-  }
-
-  onSubmit = (e) => {
+    }, 
+    [lenguages]
+  )
+  
+  const onSubmit = e => {
     e.preventDefault()
 
     const article = {
-      lenguage: this.state.lenguage,
-      title: this.state.title,
-      description: this.state.description,
-      date: this.state.date
+      lenguage: lenguage,
+      title: title,
+      description: 'blahblahba',
+      date: date
     }
 
     axios.post('http://localhost:5000/articles/add', article)
     .then( res => console.log( 'respuesta ',res.data ) )
     .catch( err => console.log( err ))
 
-    this.props.history.push('/dashboard')
+    props.history.push('/dashboard')
   }
 
-  render() {
-    const { moveContentValue } = this.props
-    const value = moveContentValue ?
-    "60px" :  "250px"
+  const moveContentValue = useSelector(store => store.main.moveContentValue)
+  const margin = moveContentValue ?
+  "60px" :  "250px"
 
-    return (
-      <div className="wrapper content" style={{ marginLeft: value }}>
-        <h3 className="">Create New Article</h3>
-        <form onSubmit={ this.onSubmit } className="">
-          <div className="form-group">
-            <input
-              placeholder='Title'
-              type='text'
-              required
-              value={ this.state.title }
-              onChange={ this.onChangeTitle }
+  console.log('render', lenguages)
+
+  return (
+    
+    <div className="wrapper content" style={{ marginLeft: margin }}>
+      <h3 className="">Create New Article</h3>
+      <form onSubmit={ onSubmit } className="">
+        <div className="form-group">
+          <input
+            placeholder='Title'
+            type='text'
+            required
+            value={ title }
+            onChange={ e => setTitle(e.target.value) }
             />
-          </div>
+        </div>
 
-          <div className="form-group">
-            <textarea
-              placeholder='Write your article...'
-              type='text'
-              required
-              rows="8"
-              value={ this.state.description }
-              onChange={ this.onChangeDescription }
-            />
-          </div>
+        <div className="form-group">
+          <textarea
+            placeholder='Write your article...'
+            type='text'
+            required
+            rows="8"
+            value={ description }
+            onChange={ e => setDescription(e.target.value) }
+            
+          />
+        </div>
 
-          <div className='form-group'>
-            <select
-              ref='userInput'
-              required
-              value={ this.state.lenguage }
-              onChange={ this.onChangeLenguage }
-              >{
-                this.state.lenguages.map( function(lenguage) {
-                return (<option
-                  key={ lenguage }
-                  value={ lenguage }
-                >
-                { lenguage }
-                </option>)
-              })
+        <div className='form-group'>
+          <select
+            ref={lenguageRef}
+            required
+            value={ lenguage }
+            onChange={ e => setLenguage(e.target.value) }>
+            {console.log('select',lenguages)}
+            {
+              lenguages.map( lenguage => <option key={ lenguage } value={ lenguage }>{ lenguage }</option> )
             }
-            </select>
-          </div>
+          </select>
+        </div>
 
-          <div className="form-group">
-            <div>
-              <DatePicker
-                selected={ this.state.date }
-                onChange={ this.onChangeDate }
-              />
-            </div>
+        <div className="form-group">
+          <div>
+            <DatePicker
+              selected={ date }
+              onChange={ e => setDate(e) }
+            />
           </div>
+        </div>
 
-          <div className="form-group">
-            <input 
-              type="submit" 
-              className="btn btn-primary"
-              value="Create" />
-          </div>
-        </form>
-      </div>
-    )
-  }
+        <div className="form-group">
+          <input 
+            type="submit" 
+            className="btn btn-primary"
+            value="Create" />
+        </div> 
+      </form>
+    </div>
+  )
 }
 
-CreateArticle.propTypes = {
-  moveContentValue: PropTypes.bool,
-}
-
-const mapStateToProps = (state) => ({
-  moveContentValue: state.main.moveContentValue
-})
-
-export default  connect(mapStateToProps, null)(CreateArticle)
+export default withRouter(CreateArticle)
